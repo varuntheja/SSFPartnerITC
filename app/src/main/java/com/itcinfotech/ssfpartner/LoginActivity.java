847ec5578bc3a7f667530545f5af63f75c7044a2
Mukesh Sharma <magarwal16@gmaill.com>
2018-12-08 12:02:00 +0530

    test commit

--- app/src/main/java/com/itcinfotech/ssfpartner/LoginActivity.java
+++ app/src/main/java/com/itcinfotech/ssfpartner/LoginActivity.java
@@ -23,6 +23,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
 
         initViews();
+
     }
 
     private void initViews(){
